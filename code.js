af0459370244cc25d7ce52f2e66165bdf2db0477
MohammedAbdul20 <mohdabdulilla786@gmail.com>
2026-06-22 16:39:21 +0530

    A lil bit spelling mis

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ for(let i = 1; i < 21; i++){
         console.log(`${i} is FizzBuzz i.e no is divisible by 3 and 5`);
     }
     else if (i % 3 == 0){
-        console.log(`${i} is Fizz i.e the number si divisible by 3`);
+        console.log(`${i} is Fizz i.e the number is divisible by 3`);
     }
     else if(i % 5 == 0){
         console.log(`${i} is Buzz`);
