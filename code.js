58e38927bb448fbc65bbca27118b1987d4e6888d
MohammedAbdul20 <mohdabdulilla786@gmail.com>
2026-06-22 15:11:32 +0530

    the branch two changes commit

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 //for loop fizzbuzz prob
 for(let i = 1; i < 21; i++){
     if (i % 3 == 0 && i % 5 == 0){
-        console.log(`${i} is FizzBuzz`);
+        console.log(`${i} is FizzBuzz i.e number is divisible by both 3,5`);
     }
     else if (i % 3 == 0){
         console.log(`${i} is Fizz`);
