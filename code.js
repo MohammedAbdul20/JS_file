e12f1cd9345c5b15006f38129cb3a16a8aaeed5f
MohammedAbdul20 <mohdabdulilla786@gmail.com>
2026-06-22 14:58:46 +0530

    the initial code

---
+++ code.js
@@ -0,0 +1,15 @@
+//for loop fizzbuzz prob
+for(let i = 1; i < 21; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log(`${i} is FizzBuzz`);
+    }
+    else if (i % 3 == 0){
+        console.log(`${i} is Fizz`);
+    }
+    else if(i % 5 == 0){
+        console.log(`${i} is Buzz`);
+    }
+    else{
+        console.log(i);
+    }
+}
